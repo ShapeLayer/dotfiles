b0ff7eb2ed4a4ba232e766a82bd76b9e374333f7
Park, Jonghyeon <belline0124@gmail.com>
2024-06-28 01:37:45 +0900

    Add iterm and oh my zsh configure

---
+++ shell.style/iterm2.sh
@@ -0,0 +1,12 @@
+brew install iterm2
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc.env
+echo "source ~/.zshrc.env" >> ~/.zshrc
+cp .p10k.zsh ~/.p10k.zsh
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# Change
+# ZSH_THEME="powerlevel10k/powerlevel10k"
+vi ~/.zshrc
+exec zsh
