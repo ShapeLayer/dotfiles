342db49a02ac78f27045ccf7982d60471347f347
Park, Jonghyeon <belline0124@gmail.com>
2024-07-02 00:27:17 +0900

    Add install script lazynvim at windows

---
+++ shell.posh/nvim.ps1
@@ -0,0 +1,4 @@
+# Requires Scoop
+scoop install neovim
+echo "New-Alias vi nvim" >> $env:USERPROFILE\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+echo "New-Alias vim nvim" >> $env:USERPROFILE\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
