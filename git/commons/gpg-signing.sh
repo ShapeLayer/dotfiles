62d7216c61b43ae1427fb65ec1a5fdf713663405
Jonghyeon Park <belline0124@gmail.com>
2024-07-29 13:20:48 +0900

    Update gpg-signing.sh

--- git/commons/gpg-signing.sh
+++ git/commons/gpg-signing.sh
@@ -2,3 +2,6 @@ gpg --full-generate-key
 gpg --list-secret-keys --keyid-format=long
 gpg --armor --export [keyid]
 git config --global commit.gpgsign true
+git config --global user.signingkey [keyid]
+git config --global tag.pgpsign true
+git config --global program [pgppath]
